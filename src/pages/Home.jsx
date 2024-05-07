bfbe35fe77ea6c6dd22278fb5504963e89aabafc
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-07 12:09:52 -0300

    Recebendo os valores da api nos componentes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import MovieCard from '../components/MovieCard';
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
 
 function Home() {
+
+    const [popularMovies, setPopularMovies] = useState([])
+
+    const getPopularMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setPopularMovies(data.results)
+    }
+
+    useEffect(() => {
+
+        const popularURL = `${moviesURL}popular?${apiKey}`
+
+        getPopularMovies(popularURL)
+    }, [])
+
     return (
-        <div>Home</div>
+        <div className='container'>
+            <h2 className='title'>Filmes Populares</h2>
+            <div className="movies-container">
+                {popularMovies.length > 0 && popularMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
     )
 }
 
