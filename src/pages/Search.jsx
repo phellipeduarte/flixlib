b8d03d870bcf6e4bc17dfab0277e13e0c415e12f
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-09 20:03:54 -0300

    Criando malha de filmes pós pesquisa

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -30,7 +30,7 @@ function Search() {
         <>
             <div className='container'>
                 <h2 className='title'>Resultados para: <span className="query-text">"{query}"</span></h2>
-                <div className="movies-container">
+                <div className="movies-container movies-container-grid">
                     {movies.length === 0 && <p>Carregando...</p>}
                     {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
                 </div>
