390f6280b51bc2189dcdc4a24e36116cd856c197
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-09 19:49:55 -0300

    Aplicando busca

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 import { BiSearchAlt2 } from 'react-icons/bi'
 import { MdLocalMovies } from "react-icons/md";
 import "./Navbar.css";
 
 function Navbar() {
+
+    const [search, setSearch] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!search) return
+
+        navigate(`/search?q=${search}`)
+        setSearch("")
+    }
+
     return (
         <>
             <nav id="navbar">
                 <h2>
                     <Link to="/"><MdLocalMovies />FlixLib</Link>
                 </h2>
-                <form>
-                    <input type="text" placeholder='Buscar' />
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Buscar" onChange={(e) => setSearch(e.target.value)} value={search} />
                     <button type="submit">
                         <BiSearchAlt2 />
                     </button>
