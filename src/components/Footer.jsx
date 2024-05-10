dce3f64c5708a6e919a4eb4d2a705cc1904451a2
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-10 11:20:18 -0300

    Acrescentando finalização de página

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            <p>&#169; 2024 FlixLib</p>
+            <p>Feito por Phellipe Duarte</p>
+            <p>Conheça meu <a href="https://github.com/phellipeduarte" target='_blank'>github</a></p>
+        </footer>
+    )
+}
+
+export default Footer
